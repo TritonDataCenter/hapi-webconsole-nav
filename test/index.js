3fa60df81a89a25f1960fa4091ba651317d07d74
David Neal <david@reverentgeek.com>
2018-04-03 11:59:38 -0400

    uses renamed webconsole-cloudapi-client

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ const Hapi = require('hapi');
 const Lab = require('lab');
 const StandIn = require('stand-in');
 const Navigation = require('../');
-const CloudApi = require('cloudapi-client');
+const CloudApi = require('webconsole-cloudapi-client');
 
 
 const schema = Fs.readFileSync(Path.join(__dirname, '../lib/schema.graphql'));
